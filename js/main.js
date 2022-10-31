4168d9f744bc2e5fb1dcefd8eb8c35ea0ff5a660
Iyo Todaka <iyo.todaka@gmail.com>
2022-10-31 05:13:16 -0700

    ver0.9

--- js/main.js
+++ js/main.js
@@ -7,16 +7,17 @@ const game_img =document.getElementById("game_image");
 const text1 =document.getElementById("inner_text1");
 const text2 =document.getElementById("inner_text2");
 
-const choice1 =document.getElementById("choice1");
-const choice2 =document.getElementById("choice2");
-const choice3 =document.getElementById("choice3");
-const choice4 =document.getElementById("choice4");
+// const choice1 =document.getElementById("choice1");
+// const choice2 =document.getElementById("choice2");
+// const choice3 =document.getElementById("choice3");
+// const choice4 =document.getElementById("choice4");
 const a_choice1 =document.getElementById("a_choice1");
 const a_choice2 =document.getElementById("a_choice2");
 const a_choice3 =document.getElementById("a_choice3");
 const a_choice4 =document.getElementById("a_choice4");
 
 let hole_flag = 0;
+let looked_candle=0;
 
 
 //gameListにaxiosを使ってGET_URLから取得したObject型のdateが格納される
@@ -25,14 +26,83 @@ let gameList;
 axios.get(GET_URL).then(({data})=>{
     //data.gamesのgamesはjsonblobに登録している配列の名前
     gameList =data.games;
-    console.log(gameList);
+
 })
 
+function OnLinkClick(roomName){
 
-function OnLinkClick(id){
+    if(roomName=="hole"){
+        //穴flagを確認する
 
-    target =document.getElementById("choices");
+        if(hole_flag==0){
+            roomName="hole1"
+            hole_flag=1
+        }else{
+            roomName="hole2"
+        }
+    }else if(roomName=="candle"){
+        //キャンドルを見たかのflag、エンディング分岐してもいいかも
+        looked_candle=1;
+    }else if(roomName=="panel"){
+        pass1 = window.prompt("What is the color of the stone", "")
+        if(pass1=="Blue"){
+            pass2 = window.prompt("What is the color of the candle", "")
+            if(pass2=="Red"){
+                roomName="end3"
+            }else if(pass2=="White"){
+                roomName="end2"
+            }else{
+                window.alert("The door was not opened")
+                return
+            }
+        }else if(pass1=="White"){
+            pass2 = window.prompt("What is the color of the candle", "")
+            if(pass2=="White"){
+                roomName="end1"
+            }else{
+                window.alert("The door was not opened")
+                return
+            }
+        }else{
+            window.alert("The door was not opened")
+            return
+        }
+    }
+    console.log(roomName)
     
+    //下のfindRoomByIdでどの部屋か判定する
+    const room = findRoomById(roomName);
 
+    //選択肢のテキストを変更する
+    a_choice1.innerHTML= room.choice1;
+    a_choice2.innerHTML= room.choice2;
+    a_choice3.innerHTML= room.choice3;
+    a_choice4.innerHTML= room.choice4;
+
+    console.log(room)
+    //選択肢に紐づくリンクを変更する
+    a_choice1.setAttribute ('href',room.choice1_name);
+    a_choice2.setAttribute ('href',room.choice2_name);
+    a_choice3.setAttribute ('href',room.choice3_name);
+    a_choice4.setAttribute ('href',room.choice4_name);
+
+    //divタグ内のimg要素のイメージURLを変更する
+    game_img.style.backgroundImage='url('+room.img+')'
+
+    //画面テキストの表示を変更する
+    text1.innerHTML = room.text1;
+    text2.innerHTML = room.text2;
+
+
+}
+
+
+function findRoomById(roomName){
+
+    for(room of gameList){
+        if(room.name ==roomName){
+            return room
+        }
+    }
 
 }
